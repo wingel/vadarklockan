b7dfaa14b59c1aac70d881cbfe41adaae0072f03
Christer Weinigel <christer@weinigel.se>
2022-10-04 12:57:22 +0200

    fix merge

--- examples/python/algotest.py
+++ examples/python/algotest.py
@@ -1,17 +1,34 @@
-#from __future__ import division, print_function, unicode_literals
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
 
-import machine, time, network
-import binascii
-import json
+from __future__ import division, print_function, unicode_literals
+
+""" Försök till clock selection algorithm implementerad i Python som
+ska illustrera de viktiga koncepten för en implementation i andra
+språk.
+
+Mål:
+
+ska helst gå att köra singeltrådat
+implementationen kanske kan vara trådad under skalet.
+
+"""
+
+import os
 import sys
 import json
 import random
-import os
-import gc
+from datetime import datetime, timedelta, timezone
+
+if __name__ == '__main__' and not sys.argv[0]:
+    print()
 
 from pyroughtime import RoughtimeClient
 
-from overlap import OverlapAlgorithm, OptimizedOverlapAlgorithm
+from overlap import OverlapAlgorithm
+from overlap_ref import RefOverlapAlgorithm
+
+statf = open('stat.json', 'a')
 
 class Server(object):
     """Glue betweeen roughtime and measurements handed to the algorithm"""
@@ -24,7 +41,7 @@ class Server(object):
         self.newver = j.get('newver', False)
         self.addr, self.port = j['addresses'][0]['address'].split(':')
         self.port = int(self.port)
-        self.pubkey = binascii.a2b_base64(j['publicKey'])
+        self.pubkey = j['publicKey']
 
     def __repr__(self):
         return "%s:%s" % (self.addr, self.port)
@@ -34,19 +51,28 @@ class Server(object):
 
         self.transmit_time = repl['start_time']
         self.receive_time = self.transmit_time + repl['rtt']
-        self.remote_time = repl['timestamp']
+        self.remote_time = repl['datetime']
         # TODO check that radi >= 0
         self.radi = repl['radi']*1E-6
+
+        statf.write(json.dumps({
+            'server': repr(self),
+            'transmit_time' : self.transmit_time,
+            'receive_time' : self.receive_time,
+            'remote_time' : self.remote_time.replace(tzinfo=timezone.utc).timestamp(),
+            'radi' : self.radi,
+            }) + '\n')
+        statf.flush()
 
     def get_meas(self):
         # Round trip time
         rtt = self.receive_time - self.transmit_time
 
         # Local clock midway between transmit_time and receive_time
-        local_time = self.transmit_time + rtt / 2
+        local_time = datetime.utcfromtimestamp(self.transmit_time + rtt / 2)
 
         # Calculate a local clock adjustment
-        adjustment = self.remote_time - local_time
+        adjustment = (self.remote_time - local_time).total_seconds()
 
         # The uncertainty is half the round trip time plus the server uncertainty (radi)
         uncertainty = rtt / 2 + self.radi
@@ -60,19 +86,6 @@ class Server(object):
         # Return lowest and highest adjustments
         return adjustment - uncertainty, adjustment + uncertainty
 
-def random_shuffle(seq):
-    l = len(seq)
-    for i in range(l):
-        j = random.randrange(l)
-        seq[i], seq[j] = seq[j], seq[i]
-
-def format_time(t):
-    tint = int(t)
-    tfrac = t - tint
-    gm = time.gmtime(tint)
-    return "%04u-%02u-%02u %02u:%02u:%06.3f" % (
-        gm[0], gm[1], gm[2], gm[3], gm[4], gm[5] + tfrac)
-
 def main():
     # Query a list of servers in a JSON file.
     with open('ecosystem.json') as f:
@@ -82,27 +95,29 @@ def main():
 
     servers = [ Server(cl, _) for _ in servers ]
 
+    # servers = servers[:6]
+
     # Try all servers a couple of times simulating that we have more
     # The first 7 servers should be good, the rest are falsetickers,
     # so use the good ones more often so that we will have a majority
     # of good ones.
-    servers = servers[:7] + servers[7:12]
+    servers = servers[:7] * 3 + servers * 2
 
-    random_shuffle(servers)
+    random.shuffle(servers)
 
     # We want a majority where at least this many servers agree
-    wanted = 5
+    wanted = 10
 
-    algorithm = OptimizedOverlapAlgorithm()
+    algorithm = OverlapAlgorithm()
+    ref_algorithm = RefOverlapAlgorithm()
+
+    statf.write('\n')
+    statf.flush()
 
     responses = 0
-    last_res = 0
+    last_count = 0
 
     for server in servers:
-        # Force garbage collection to run
-        gc.collect()
-        print("memory free", gc.mem_free())
-
         if 0 and server.addr.startswith('false'):
             continue
 
@@ -115,61 +130,39 @@ def main():
             local_time, rtt, adjustment, uncertainty = server.get_meas()
 
             print('%-38s local %s remote %s adj %9.0f us rtt %7.0f us radi %7.0f us' % (
-                server, format_time(local_time), format_time(server.remote_time),
+                server, local_time, server.remote_time,
                 adjustment * 1E6, rtt * 1E6, server.radi * 1E6))
 
         except Exception as e:
             print('%-38s Exception: %s' % (server, e))
-            print(e.__class__)
-            if 0:
-                raise
             continue
 
         responses += 1
         lo, hi = server.get_adjustment_range()
-        res = algorithm.process(lo, hi)
-        if res >= responses // 2 and res >= wanted:
-            adjustment = (algorithm.hi + algorithm.lo) / 2
-            uncertainty = (algorithm.hi - algorithm.lo) / 2
+        algorithm.add(lo, hi)
+        ref_algorithm.add(lo, hi)
+        count, lo, hi = algorithm.find()
+        ref_count, ref_lo, ref_hi = ref_algorithm.find()
+        assert count, ref_count
+        assert lo == ref_lo
+        assert hi == ref_hi
+        if count > responses // 2 and count >= wanted:
+            adjustment = (hi + lo) / 2
+            uncertainty = (hi - lo) / 2
 
             print("success with %u/%u clocks adjust %+.0f us uncertainty %.0f us" % (
-                res, responses,
+                count, responses,
                 adjustment * 1E6,
                 uncertainty * 1E6))
             print()
 
-            if 1:
-                # This is a good place to set the clock and break out of the loop
-                t0 = time.time()
-                tn = t0 + adjustment
-                time.settime(tn)
-                t1 = time.time()
-                print("adjust %+.0f us" % (adjustment * 1E6))
-                print("t0", format_time(t0), t0)
-                print("tn", format_time(tn), tn)
-                print("t1", format_time(t1), t1)
-                break
+            # This is a good place to set the clock and break out of the loop
 
             # Getting more responses should always give more overlaps, never fewer
-            assert last_res <= res
-            last_res = res
+            assert last_count <= count
+            last_count = count
 
             # assert algorithm.adjustment < 1E-3
 
-# Set up WIFI on ESP32
-
-import wifi_config
-
-sta = network.WLAN(network.STA_IF)
-if not sta.active():
-    sta.active(True)
-
-    # print(sta.scan())
-    sta.connect(wifi_config.WLAN_SSID, wifi_config.WLAN_PASSWORD)
-
-while not sta.isconnected():
-    pass
-
-print(sta.ifconfig())
-
-main()
+if __name__ == '__main__':
+    main()
